77a909afa59b14e96ddbfb02b07f1387f27b6be5
Cyrine larbi <cyrino.arbi21@gmail.com>
2018-09-14 15:59:38 +0100

    0.1

---
+++ README.md
@@ -0,0 +1,24 @@
+# Credit Card
+
+## Vue.js credit card 
+
+### Dependencies:
+- [Vue.js](https://vuejs.org/)
+- [Pug.js](https://pugjs.org/)
+- [Stylus](http://stylus-lang.com/)
+- [Payment](https://github.com/jessepollak/payment)
+
+
+## Contribute
+
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+```
+## Support:
+- [cyrine larbi](https://github.com/cyrinearbi) - cyrine.arbi@esprit.tn
---
+++ example/App.vue
@@ -0,0 +1,69 @@
+<template lang="pug">
+  v-app
+    v-container
+      v-layout(row wrap)
+        .title Credit Card
+
+      br
+      v-layout(row wrap)
+        v-flex(xs12 md6)
+          v-layout(row wrap)
+            v-flex.pr-2(xs12 md6)
+              v-text-field(
+                name="number"
+                label="Card number"
+                v-model="cardDetail.number"
+                )
+            v-flex(xs12 md6)
+              v-text-field(
+                name="name"
+                label="Full Name"
+                v-model="cardDetail.name"
+                )
+          v-layout(row wrap)
+            v-flex.pr-2(xs12 md6)
+              v-text-field(
+                name="expiry"
+                label="MM/YY"
+                v-model="cardDetail.expiry"
+                )
+            v-flex(xs12 md6)
+              v-text-field(
+                name="name"
+                label="CVC"
+                v-model="cardDetail.cvc"
+                @focus="invertedCard = true"
+                @blur="invertedCard = false"
+                )
+        v-spacer
+        v-flex(xs12 md3)
+          card(
+            v-model="cardDetail"
+            :invert-card.sync="invertedCard"
+            format-data
+            )
+</template>
+
+<script>
+import Card from '@/components/Card'
+
+const defaultCreditCardData = {
+  number: '6703 0400 0000 0001 3',
+  name: 'Owner name',
+  expiry: '042019',
+  cvc: '154'
+}
+
+export default {
+  name: 'card-example',
+
+  components: {
+    Card
+  },
+
+  data: () => ({
+    cardDetail: defaultCreditCardData,
+    invertedCard: false
+  })
+}
+</script>
